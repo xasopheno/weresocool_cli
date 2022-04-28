239cebcac195be5066d1b4ecb3652986c34ac5c3
xasopheno <danny@xasopheno.com>
2022-04-27 23:06:58 -0400

    WIP

--- src/watch.rs
+++ src/watch.rs
@@ -1,5 +1,5 @@
 use crate::Error;
-use notify::{watcher, RecursiveMode, Watcher};
+use notify::{RecommendedWatcher, RecursiveMode, Watcher};
 use std::path::Path;
 use std::path::PathBuf;
 use std::sync::mpsc::channel;
@@ -16,30 +16,30 @@ pub fn watch(
     render_manager: Arc<Mutex<RenderManager>>,
 ) -> Result<(), Error> {
     std::thread::spawn(move || -> Result<(), Error> {
-        loop {
-            let (tx, rx) = channel();
+        // loop {
+        let (tx, rx) = channel();
 
-            let mut watcher = watcher(tx, Duration::from_millis(10))?;
+        let mut watcher = RecommendedWatcher::new(tx).unwrap();
 
-            let path = Path::new(&working_path).join(Path::new(&filename));
+        let path = Path::new(&working_path).join(Path::new(&filename));
 
-            watcher.watch(path, RecursiveMode::NonRecursive)?;
-            if let Ok(_event) = rx.recv() {
-                // println!("{:?}", event);
-                let render_voices =
-                    match prepare_render_outside(Filename(&filename), Some(working_path.clone())) {
-                        Ok(result) => Some(result),
-                        Err(error) => {
-                            println!("{}", error);
-                            None
-                        }
-                    };
+        watcher.watch(path.as_ref(), RecursiveMode::NonRecursive)?;
+        if let Ok(event) = rx.recv() {
+            println!("{:?}", event);
+            let render_voices =
+                match prepare_render_outside(Filename(&filename), Some(working_path.clone())) {
+                    Ok(result) => Some(result),
+                    Err(error) => {
+                        println!("{}", error);
+                        None
+                    }
+                };
 
-                if let Some(voices) = render_voices {
-                    render_manager.lock().unwrap().push_render(voices);
-                }
+            if let Some(voices) = render_voices {
+                render_manager.lock().unwrap().push_render(voices);
             }
         }
+        Ok(())
     });
     Ok(())
 }
